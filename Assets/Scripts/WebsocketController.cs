4c3c7b383922cafe4463fdf4ec268aca35bc851b
nokkii <nokkii.03@gmail.com>
2019-03-31 10:55:02 +0900

    スマホコントローラ機能完成

--- Assets/Scripts/WebsocketController.cs
+++ Assets/Scripts/WebsocketController.cs
@@ -9,17 +9,20 @@ namespace nmxi.websocket
 {
     public class WebsocketController : MonoBehaviour
     {
+        [SerializeField]
+        GameObject camera;
+        static RotationCamera rotationCamera;
 
-        // Use this for initialization
         void Start()
         {
             if (GetComponent<EasyWSServer>())
             {
                 GetComponent<EasyWSServer>().StartWebSocketServer();
             }
+            
+            rotationCamera = camera.GetComponent<RotationCamera>();
         }
 
-        // Update is called once per frame
         void Update()
         {
 
@@ -27,7 +30,33 @@ namespace nmxi.websocket
 
         public void OnReceiveBytes(byte[] b)
         {
-            Debug.Log(System.Text.Encoding.GetEncoding("UTF-8").GetString(b));
+            string command = System.Text.Encoding.GetEncoding("UTF-8").GetString(b);
+            if(command == "spring")
+            {
+                rotationCamera.Theta = 135;
+                rotationCamera.StopRotate();
+            }else if(command == "summer")
+            {
+                rotationCamera.Theta = 225;
+                rotationCamera.StopRotate();
+            }else if(command == "autumn")
+            {
+                rotationCamera.Theta = 315;
+                rotationCamera.StopRotate();
+            }else if(command == "winter")
+            {
+                rotationCamera.Theta = 45;
+                rotationCamera.StopRotate();
+            }else if(command == "togglemove")
+            {
+                rotationCamera.ToggleRotate();
+            }else if(command == "nextscene")
+            {
+                // TODO:next scene
+            }else{
+
+            }
+            //Debug.Log(command);
         }
     }
 }
